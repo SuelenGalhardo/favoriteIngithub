f91f581c5cccdd373d3efc4f46ece1c0314793d8
SuelenGalhardo <suelenranucci@gmail.com>
2024-05-28 12:32:52 +0200

    added fetch json

--- js/pages/Favorites.js
+++ js/pages/Favorites.js
@@ -1,9 +1,42 @@
+
+export class GithubUser {
+    static search(username) {
+
+        const url = `https://api.github.com/users/${username}`;
+      
+        return fetch(url)
+           .then(response => response.json())
+           .then((data) => {
+            if (data.message === 'Not Found') {
+                throw new Error('Usuário não encontrado');
+            }
+
+           //const {login, name, public_repos, followers} = data;
+
+           return {
+            login: data.login,  
+            name: data.name, 
+            public_repos: data.public_repos, 
+            followers: data.followers
+        };
+      
+
+    }
+           )}
+
+
+};
+
+
 //import { githubUser } from "./pages/githubUser";
 
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
+
+    GithubUser.search('SuelenGalhardo')
+    .then(user => console.log(user));
   }
   load() {
     const entries = JSON.parse(localStorage.getItem('@github-favorites')) || [];
